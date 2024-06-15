61b0b7f2d54a0adf6ca40dcc1cc8d014eacc6048
jcerveto <joan.cerveto@ua.es>
2024-06-15 13:53:07 +0200

    Basic http api

--- README.md
+++ README.md
@@ -7,5 +7,6 @@
 - Authentication using JWT.
 - CRUD operations for users in raw JSONL file.
 - Password hashing using bcrypt.
+- Private routes.
 - Unit tests using Jest.
 
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
 import express from 'express'
+import cors from 'cors'
+
+import { PORT } from './config'
+
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
-const PORT = 3000
 
 app.get('/ping', (_, res) => {
     console.log('ping request received')
     res.send('pong')
 })
+
+
+app.get('/', (_, res) => {
+    res.send('Hello World!')
+})
+
+
+app.post('/login', (_req, _res) => {})
+app.post('/signup', (_req, _res) => {})
+app.post('/logout', (_req, _res) => {})
+app.post('verify', (_req, _res) => {})
+app.post('/reset', (_req, _res) => {})
+app.post('private', (_req, _res) => {})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}!`)
