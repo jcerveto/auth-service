c3e0e556e72aa33a6ec0fb15533cfb02d224bc7c
jcerveto <joan.cerveto@ua.es>
2024-06-15 14:02:36 +0200

    set cors

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,23 @@
-export const {
-    PORT = 3000
-} = process.env
+export const PORT = 3000
+
+const whiteList: any[] = [
+]
+
+
+const availableMethods: string[] = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+]
+
+export const corsOptions = {
+    origin: whiteList,
+    methods: availableMethods,
+    optionsSuccessStatus: 200,
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Authorization'],
+}
 
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 
-import { PORT } from './config'
+import { PORT, corsOptions } from './config'
 
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+
+app.use(cors(corsOptions))
 
 
 app.get('/ping', (_, res) => {
